43c81067a66b4c53d7a11556f9688f9ef85bdd86
yuly <yulyg5900@gmail.com>
2025-06-26 17:29:15 -0500

    ejercisio R pithon

---
+++ codigo.py
@@ -0,0 +1,10 @@
+# Programa para saber si un número es par o impar
+
+# Entrada del usuario
+numero = int(input("Ingrese un número: "))
+
+# Verificación
+if numero % 2 == 0:
+    print(f"El número {numero} es PAR.")
+else:
+    print(f"El número {numero} es IMPAR.")
